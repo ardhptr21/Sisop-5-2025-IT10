e43d4655543062a6f61c1a4125ec35b7ebfbdf75
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-05-28 15:04:35 +0700

    create initialize string functionality

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,53 @@
-#include "shell.h"
 #include "kernel.h"
 
+#include "shell.h"
+
 int main() {
-  clearScreen();
-  shell();
+    clearScreen();
+    shell();
+    return 0;
 }
 
-void printString(char *str)
-{
-  //TODO: Implementasi fungsi untuk mencetak string
+void printString(char *str) {
+    char *p = str;
+    while (*p != 0) {
+        if (*p == '\n') {
+            interrupt(0x10, 0x0e0d, 0x000F, 0, 0);
+            interrupt(0x10, 0x0e0a, 0x000F, 0, 0);
+        } else {
+            interrupt(0x10, 0x0e00 + *p, 0x000F, 0, 0);
+        }
+        p++;
+    }
 }
 
-void readString(char *buf)
-{
-  //TODO: Implementasi fungsi untuk membaca string
+void readString(char *buf) {
+    char c;
+    int i = 0;
+
+    while (1) {
+        c = interrupt(0x16, 0, 0, 0, 0);
+
+        if (c == '\r') {
+            interrupt(0x10, 0x0e0d, 0x000F, 0, 0);
+            interrupt(0x10, 0x0e0a, 0x000F, 0, 0);
+            break;
+        } else if (c == '\b') {
+            if (i > 0) {
+                i--;
+                interrupt(0x10, 0x0e08, 0x000F, 0, 0);
+                interrupt(0x10, 0x0e20, 0x000F, 0, 0);
+                interrupt(0x10, 0x0e08, 0x000F, 0, 0);
+            }
+        } else {
+            interrupt(0x10, 0x0e00 + c, 0x000F, 0, 0);
+            buf[i++] = c;
+        }
+    }
+    buf[i] = 0;
 }
 
-void clearScreen()
-{
-  //TODO: Implementasi fungsi untuk membersihkan layar
+void clearScreen() {
+    interrupt(0x10, 0x0000, 0x0007, 0, 0);
+    interrupt(0x10, 0x0200, 0x0000, 0, 0);
 }
