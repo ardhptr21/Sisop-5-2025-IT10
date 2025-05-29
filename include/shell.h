cb8326ae1b587dad51eb6abc4a4c6d867223ac9b
Asarumawari <runnirattancraft@gmail.com>
2025-05-29 21:42:26 +0700

    Implement grand company

--- include/shell.h
+++ include/shell.h
@@ -11,6 +11,7 @@ void parseCommand(char *buf, char *cmd, char arg[2][64]);
 void yogurt(char *buf);
 void realyogurt();
 void changeUser(char *user, char *to);
+void grandCompany(char *arg, char *user);
 void calc(char *op, char *a, char *b);
 
 #endif  // __SHELL_H__
